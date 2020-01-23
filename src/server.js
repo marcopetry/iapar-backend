6944202b2157ecb34821b76dbf254ae2b960cb4f
marcopetry <marcomattospetry@gmail.com>
2020-01-23 14:23:25 -0300

    Instalado cors e habilitado acesso a todas as requisições.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes/users-route');
+const cors = require('cors');
 const controller = require('./controllers/users-controllers');
 
 require('./database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
